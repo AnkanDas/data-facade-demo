0da4c2b582dfd9b74b9ede4536e9cd2a89d298fd
AnkanDas <32011548+AnkanDas@users.noreply.github.com>
2022-08-14 20:14:44 +0530

    Update first_class_table_9_columns.sql

--- macros/first_class_table_9_columns.sql
+++ macros/first_class_table_9_columns.sql
@@ -1,4 +1,4 @@
-% macro nine_columns(table1, column_1, column_2, column_3, column_4, column_5, column_6, column_7, column_8, column_9 ) %}
+{% macro nine_columns(table1, column_1, column_2, column_3, column_4, column_5, column_6, column_7, column_8, column_9 ) %}
 
 select {{ column_1 }}, {{ column_2 }}, {{ column_3 }}, {{ column_4 }}, {{ column_5 }}, {{ column_6 }}, {{ column_7 }}, {{ column_8 }}, {{ column_9 }} from {{ table1 }}
 
