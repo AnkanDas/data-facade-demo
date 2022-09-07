589c464fab763715cf537ff61528b3d62341504f
AnkanDas <ankan.das198@gmail.com>
2022-09-07 14:01:14 +0530

    new action

--- macros/add_datediff_column.sql
+++ macros/add_datediff_column.sql
@@ -1,6 +1,6 @@
-{% macro table_join(table1, new_column, column1, column2) %}
+{% macro calcolumn(table, new_column, column1, column2) %}
 
-ALTER TABLE {{table1}}
+ALTER TABLE {{table}}
 
 ADD {{new_column}} AS DATEDIFF(minute,{{column1}},{{column2}})
 
