d8774e0ec201d85b42d13d79a5e9c9260e3ff403
AnkanDas <ankan.das198@gmail.com>
2022-09-13 16:26:31 +0530

    Update group_by_2_column.sql

--- macros/group_by_2_column.sql
+++ macros/group_by_2_column.sql
@@ -1,6 +1,6 @@
 {% macro sel_2_col_group(table1, column_x, column_y) %}
 
-select {{ column_x }}, AVG({{ column_y }}) 
+select {{ column_x }}, AVG({{ column_y }}) as {{ column_y }}
 from {{ table1 }}
 GROUP BY {{column_x}}
 
