26d7b4ac67b46d7be6788ba1a4c6fd665f3d50bf
AnkanDas <ankan.das198@gmail.com>
2022-10-04 01:30:49 +0530

    Update select_3_columns_groupby.sql

--- macros/select_3_columns_groupby.sql
+++ macros/select_3_columns_groupby.sql
@@ -1,4 +1,4 @@
-{% macro sel_3_column_groupby(table1, column_x, column_y, column_z) %}
+{% macro sel_3_column_groupby(table1, column_x, column_y, column_z, total_column) %}
 
 select {column_x}, {column_y}, sum({column_z}) as {total_column} from {table1}
 Group by {column_x}, {column_y}
