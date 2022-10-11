c657ee1c394b27da11beee21269bb18e396a5a31
AnkanDas <ankan.das198@gmail.com>
2022-10-11 14:36:07 +0530

    Update alter_table_drop_column.sql

--- macros/alter_table_drop_column.sql
+++ macros/alter_table_drop_column.sql
@@ -1,6 +1,11 @@
 {% macro drop_column(table1, column1) %}
 
-ALTER TABLE table1
-  DROP COLUMN column1;
+SELECT * INTO temptable
+FROM ixigo_analytics_data;
+/* Drop the columns that are not needed */
+ALTER TABLE temptable
+DROP COLUMN {column1};
+/* Get results and drop temp table */
+SELECT * FROM temptable;
 
   {% endmacro %}
